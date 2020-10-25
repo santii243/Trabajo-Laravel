f68b36041b9f9a4519b99c0a93f8e37bcfd4dadf
ignacio <ignacastrisa@gmail.com>
2020-10-25 20:53:29 +0100

    factorias realizadas

--- database/factories/worksheetFactory.php
+++ database/factories/worksheetFactory.php
@@ -4,9 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\worksheet::class, function (Faker $faker) {
     return [
-        'student_id' => \App\student::all()->random()->id,
+        
         'date' => $faker -> date($format = 'Y-m-d', $max = 'now'),
         'description' => $faker->paragraph,
+        'student_id' => \App\student::all()->random()->id,
         'accepted' => $faker->boolean,
         'deleted' => $faker->boolean,
     ];
